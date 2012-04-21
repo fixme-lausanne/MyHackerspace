c21a859944700a05ad4f91541545f85f2d69ce78
Rorist <aubort.jeanbaptiste@gmail.com>
2012-04-21 22:30:20 +0200

    Use channel to grab file (incomplete)

--- src/ch/fixme/status/Net.java
+++ src/ch/fixme/status/Net.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -15,35 +18,59 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.util.Log;
 
-public class Net
-{
+public class Net {
 
-    private final static String TAG = "Net";
+	private final static String TAG = "Net";
 
-    static public String get(String url) {
-        StringBuilder builder = new StringBuilder();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(url);
-        try {
-            HttpResponse response = client.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            if (statusCode == 200) {
-                HttpEntity entity = response.getEntity();
-                InputStream content = entity.getContent();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-            } else {
-                Log.e(TAG, "Failed to download file");
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
-    }
+	static public String get(String url) {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				Log.e(TAG, "Failed to download file: Status " + statusCode);
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
+
+	public static void fastChannelCopy(final ReadableByteChannel src,
+			final WritableByteChannel dest) throws IOException,
+			NullPointerException {
+		if (src != null && dest != null) {
+			final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+			while (src.read(buffer) != -1) {
+				buffer.flip();
+				dest.write(buffer);
+				buffer.compact();
+			}
+			buffer.flip();
+			while (buffer.hasRemaining()) {
+				dest.write(buffer);
+			}
+			if (src != null) {
+				src.close();
+			}
+			if (dest != null) {
+				dest.close();
+			}
+		}
+	}
+
 }
