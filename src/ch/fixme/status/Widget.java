057e0e86aa1831598b5e44be56c371cf9195f3eb
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2012-05-04 00:58:40 +0200

    According to doc better set update to 0 if using AlarmManager

--- src/ch/fixme/status/Widget.java
+++ src/ch/fixme/status/Widget.java
@@ -20,7 +20,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 public class Widget extends AppWidgetProvider {
