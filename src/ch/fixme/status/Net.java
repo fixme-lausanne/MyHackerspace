2cecb982bdab02bdacc2c0542f2e185a59432773
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2012-05-13 18:00:50 +0200

    Accept all SSL certificates (Good to test on emulator)

--- src/ch/fixme/status/Net.java
+++ src/ch/fixme/status/Net.java
@@ -10,8 +10,15 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLException;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import android.os.Build;
 
@@ -20,13 +27,21 @@ public class Net {
     private final String USERAGENT = "Android/" + Build.VERSION.RELEASE + " ("
             + Build.MODEL + ") MyHackerspace/1.4";
 
-    public Net(String urlStr, OutputStream out) throws SSLException,
-            IOException {
+    public Net(String urlStr, OutputStream out)
+            throws NoSuchAlgorithmException, KeyManagementException,
+            SSLException, IOException {
+
         // HTTP connection reuse which was buggy pre-froyo
         if (Integer.parseInt(Build.VERSION.SDK) < Build.VERSION_CODES.FROYO) {
             System.setProperty("http.keepAlive", "false");
         }
 
+        // Accept all SSL certificates
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Connect and get data
         URL url = new URL(urlStr);
         HttpURLConnection urlConnection = (HttpURLConnection) url
                 .openConnection();
@@ -44,4 +59,16 @@ public class Net {
             urlConnection.disconnect();
         }
     }
+
+    private TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+        }
+
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+        }
+    } };
 }
