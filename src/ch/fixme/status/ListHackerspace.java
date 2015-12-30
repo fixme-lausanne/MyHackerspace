99ec0281bbd2de54b43fa3e5190e22c6105a9ac0
Rorist <rorist@0xcafe.ch>
2015-12-30 16:10:54 +0100

    handle favorite

--- src/ch/fixme/status/ListHackerspace.java
+++ src/ch/fixme/status/ListHackerspace.java
@@ -16,11 +16,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.SectionIndexer;
 import com.woozzu.android.util.StringMatcher;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+
 
 public class ListHackerspace
         extends ArrayAdapter<String>
         implements SectionIndexer {
 
+    private Context mContext;
+    private SharedPreferences mPrefs;
     private List<String> mData;
     private LayoutInflater mInflater;
     private String mSections = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -28,7 +34,9 @@ public class ListHackerspace
     public ListHackerspace(Context context, int resource,
             int textViewResourceId, List<String> objects) {
         super(context, resource, textViewResourceId, objects);
-        mInflater = LayoutInflater.from(context);
+        mContext = context;
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        mInflater = LayoutInflater.from(mContext);
         mData = objects;
     }
 
@@ -46,12 +54,26 @@ public class ListHackerspace
             holder = (ViewHolder) cView.getTag();
         }
         // Set values
-        holder.img.setOnClickListener(null);
-        holder.name.setText(mData.get(pos));
+        final String hs_name = mData.get(pos);
+        holder.name.setText(hs_name);
+        holder.favorited = mPrefs.getBoolean(hs_name, false);
+        if(holder.favorited) {
+            holder.img.setImageResource(R.drawable.star_on);
+        } else {
+            holder.img.setImageResource(R.drawable.star_off);
+        }
+        holder.img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Editor edit = mPrefs.edit();
+                edit.putBoolean(hs_name, !holder.favorited);
+                edit.commit();
+            }
+        });
         return cView;
     }
 
     static class ViewHolder {
+        Boolean favorited;
         TextView name;
         ImageButton img;
     }
