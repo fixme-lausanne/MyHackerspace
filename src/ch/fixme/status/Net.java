7aa4cf9fe477b20040c1a3904771eb77781d10b4
Rorist <rorist@0xcafe.ch>
2016-04-14 22:53:22 +0200

    revert handling http->https but keep some logging

--- src/ch/fixme/status/Net.java
+++ src/ch/fixme/status/Net.java
@@ -38,11 +38,11 @@ public class Net {
     private HttpURLConnection mUrlConnection;
     private Context mCtxt;
 
-    public Net(String urlStr, final Context ctxt) throws Throwable {
+    public Net(String urlStr, Context ctxt) throws Throwable {
         this(urlStr, true, ctxt);
     }
 
-    public Net(String urlStr, boolean useCache, final Context ctxt) throws Throwable {
+    public Net(String urlStr, boolean useCache, Context ctxt) throws Throwable {
         mCtxt = ctxt;
         // register MemorizingTrustManager for HTTPS
         SSLContext sc = SSLContext.getInstance("TLS");
@@ -74,12 +74,9 @@ public class Net {
                     str.append(line);
                 }
                 return str.toString();
-            } else if(responseCode == HttpURLConnection.HTTP_MOVED_PERM || 
-                    responseCode == HttpURLConnection.HTTP_MOVED_TEMP) {
-                String newUrl = mUrlConnection.getHeaderField("Location");
-                return new Net(newUrl, mCtxt).getString();
             } else {
-                throw new Throwable(mUrlConnection.getResponseCode() + ": " + mUrlConnection.getResponseMessage());
+                throw new Throwable(mUrlConnection.getResponseMessage()
+                    + ": " + mUrlConnection.getResponseMessage());
             }
         } finally {
             if (is != null) {
@@ -97,12 +94,8 @@ public class Net {
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 is = mUrlConnection.getInputStream();
                 return BitmapFactory.decodeStream(new FlushedInputStream(is));
-            } else if(responseCode == HttpURLConnection.HTTP_MOVED_PERM || 
-                    responseCode == HttpURLConnection.HTTP_MOVED_TEMP) {
-                String newUrl = mUrlConnection.getHeaderField("Location");
-                return new Net(newUrl, mCtxt).getBitmap();
             } else {
-                throw new Throwable(mUrlConnection.getResponseCode() + ": " + mUrlConnection.getResponseMessage());
+                throw new Throwable(mUrlConnection.getResponseMessage());
             }
         } finally {
             if (is != null) {
