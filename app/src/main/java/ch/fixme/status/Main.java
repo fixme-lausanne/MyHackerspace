daf88593ceefaca8a218f4d338e054b84282798b
rorist <rorist@0xcafe.ch>
2016-12-30 14:51:14 +0100

    remove version from manifest -> its in the gradle settings, fix typo

--- app/src/main/java/ch/fixme/status/Main.java
+++ app/src/main/java/ch/fixme/status/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale
+import java.util.Locale;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Pattern;
