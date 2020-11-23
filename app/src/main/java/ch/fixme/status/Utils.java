501d9a6e34b0565c987b68b285cccc24f29c87ef
Danilo Bargen <mail@dbrgn.ch>
2020-11-23 23:52:31 +0100

    Replace parser classes with spaceapi-kt based parsing
    Besides replacing the JSON / HashMap based parser, the whole layout
    setup code was also refactored. A few helpers were added to make the
    code a bit cleaner.
    
    The widget can be added but currently doesn't seem to work on Android
    11. However, as far as I know, it did not work previously either.
    Probably some APIs changed.

---
+++ app/src/main/java/ch/fixme/status/Utils.java
@@ -0,0 +1,26 @@
+package ch.fixme.status;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Utils {
+    /**
+     * Join the strings using the specified separator.
+     */
+    public static @Nullable
+    String joinStrings(@NonNull String separator, String... strings) {
+        final StringBuilder builder = new StringBuilder();
+        boolean empty = true;
+        for (String string : strings) {
+            if (string != null) {
+                if (empty) {
+                    builder.append(string);
+                    empty = false;
+                } else {
+                    builder.append(separator).append(string);
+                }
+            }
+        }
+        return empty ? null : builder.toString();
+    }
+}
---
+++ app/src/test/java/ch/fixme/status/UtilsTest.java
@@ -0,0 +1,19 @@
+package ch.fixme.status;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UtilsTest {
+    @Test
+    public void testJoinStrings() {
+        Assert.assertEquals("foo / bar", Utils.joinStrings(" / ", "foo", "bar"));
+        Assert.assertEquals("foo.bar.baz", Utils.joinStrings(".", "foo", "bar", "baz"));
+        Assert.assertEquals("foo.baz", Utils.joinStrings(".", "foo", null, "baz"));
+        Assert.assertEquals("foo.baz", Utils.joinStrings(".", null, "foo", null, "baz"));
+        Assert.assertNull(Utils.joinStrings(" / ", null, null, null));
+        Assert.assertNull(Utils.joinStrings(" / "));
+    }
+}
