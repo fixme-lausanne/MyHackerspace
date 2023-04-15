d965b217097697ca10bb1c735d283e0100daa759
Danilo Bargen <mail@dbrgn.ch>
2023-04-16 01:08:42 +0200

    A few fixes for the about view
    - Button doesn't seem to work on API 31, remove it and replace it with
      linked URL instead
    - Fix a few layout bugs
    - Remove unused imports

--- app/src/main/java/io/spaceapi/community/myhackerspace/AboutLayout.java
+++ app/src/main/java/io/spaceapi/community/myhackerspace/AboutLayout.java
@@ -1,17 +1,8 @@
 package io.spaceapi.community.myhackerspace;
 
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -36,38 +27,14 @@ public class AboutLayout extends LinearLayout {
     }
 
     public void init() {
-        Button openRepoButton = findViewById(R.id.about_open_repo_button);
-        openRepoButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView repository = (TextView)findViewById(R.id.about_repository);
-                String url = (String) repository.getText();
-                openWebPage(url);
-            }
-        });
         TextView version = findViewById(R.id.about_version_text);
         version.setText(BuildConfig.VERSION_NAME + " (" + Integer.toString(BuildConfig.VERSION_CODE) + ")");
     }
 
     public static AboutLayout create(Context context) {
-        // use xml layout, see https://stackoverflow.com/a/13889257
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         AboutLayout about = (AboutLayout) layoutInflater.inflate(R.layout.about, null, false);
         about.init();
         return about;
     }
-
-    public void openWebPage(String url) {
-        // from https://stackoverflow.com/a/43981160
-        Uri webpage = Uri.parse(url);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-
-        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-            getContext().startActivity(intent);
-        }else{
-//Page not found
-        }
-    }
-
 }
